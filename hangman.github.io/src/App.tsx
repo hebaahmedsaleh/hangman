5b0b9661d68ddc2f89e922e21f84c008d5c8499a
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2024-06-25 14:16:03 +0300

    add home screen css

--- hangman.github.io/src/App.tsx
+++ hangman.github.io/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import Logo from "./assets/images/logo.svg";
+import PlayLogo from "./assets/images/icon-play.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+      <div className="content">
+        <img className="title" src={Logo} />
+        <div className="play__btn__container">
+          <img src={PlayLogo} alt="icon-play" />
+        </div>
+
+        <button role="link" className="button">
+          <div className="hOWTOPLAY">How to play </div>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
