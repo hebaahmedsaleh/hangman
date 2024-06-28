3085d13600cf0fb5bc41a23c27b0e1c295cda8c0
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2024-06-28 22:42:57 +0300

    add base name

--- hangman.github.io/vite.config.ts
+++ hangman.github.io/vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: "/hangman.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
