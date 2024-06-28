1934b13ccb784661105aa55a61ff1818798b223a
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2024-06-28 22:27:04 +0300

    check

--- hangman.github.io/vite.config.ts
+++ hangman.github.io/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/HANGMAN.GITHUB.IO/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
